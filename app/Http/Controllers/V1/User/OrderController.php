2f935f41c001b01974ae6dd16af090ca64c8df9a
daley7292 <daley7292@gmail.com>
2025-05-15 01:42:17 +0800

    Update OrderController.php

--- app/Http/Controllers/V1/User/OrderController.php
+++ app/Http/Controllers/V1/User/OrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\V1\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\OrderSave;
+use App\Models\CommissionLog;
+use App\Models\Coupon;
 use App\Models\Order;
 use App\Models\Payment;
 use App\Models\Plan;
@@ -12,9 +14,11 @@ use App\Services\CouponService;
 use App\Services\OrderService;
 use App\Services\PaymentService;
 use App\Services\PlanService;
+use App\Services\TelegramService;
 use App\Services\UserService;
 use App\Utils\Helper;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Library\BitpayX;
@@ -182,6 +186,7 @@ class OrderController extends Controller
         if ($order->total_amount <= 0) {
             $orderService = new OrderService($order);
             if (!$orderService->paid($order->trade_no)) abort(500, '');
+            $this->notify($order);
             return response([
                 'type' => -1,
                 'data' => true
@@ -262,5 +267,125 @@ class OrderController extends Controller
         return response([
             'data' => true
         ]);
+    }
+
+    private function notify(Order $order){
+        // type
+        $types = [1 => "新购", 2 => "续费", 3 => "变更" , 4 => "流量包"];
+        $type = $types[$order->type] ?? "未知";
+
+        // planName
+        $planName = "";
+        $plan = Plan::find($order->plan_id);
+        if ($plan) {
+            $planName = $plan->name;
+        }
+
+        // period
+        // 定义英文到中文的映射关系
+        $periodMapping = [
+            'month_price' => '月付',
+            'quarter_price' => '季付',
+            'half_year_price' => '半年付',
+            'year_price' => '年付',
+            'two_year_price' => '2年付',
+            'three_year_price' => '3年付',
+            'onetime_price' => '一次性付款',
+            'setup_price' => '设置费',
+            'reset_price' => '流量重置包'
+        ];
+        $period = $periodMapping[$order->period];
+
+        // email
+        $userEmail = "";
+        $user = User::find($order->user_id);
+        if ($user){
+            $userEmail = $user->email;
+        }
+
+        // inviterEmail  inviterCommission
+        $inviterEmail = '';
+        $getAmount = 0; // 本次佣金
+        $anotherInfo = "邀请人：该用户不存在邀请人";
+
+        if (!empty($order->invite_user_id)) {
+            $inviter = User::find($order->invite_user_id);
+            if ($inviter) {
+                $inviterEmail = $inviter->email;
+                $getAmount = $this->getCommission($inviter->id, $order); // 本次佣金
+
+                if ((int)config('v2board.withdraw_close_enable', 0)) {
+                    $inviterBalance = $inviter->balance / 100 + $getAmount; // 总余额 （关闭提现）
+                    $anotherInfo = "邀请人总余额：" . $inviterBalance. " 元";
+                } else {
+                    $inviterCommissionBalance = $inviter->commission_balance / 100 + $getAmount; // 总佣金 （允许提现）
+                    $anotherInfo = "邀请人总佣金：" . $inviterCommissionBalance. " 元";
+
+                }
+            }
+        }
+
+        $discountAmount = "无";
+        $code = "无";
+        $couponID = $order->coupon_id;
+        if ($couponID !== null){
+
+            //优惠金额
+            $discountAmount = $order->discount_amount / 100 . " 元" ;
+
+            // 优惠码
+            $coupon = Coupon::where('id', $couponID)
+                ->first();
+
+            $code = $coupon->code;
+        }
+
+        //注册日期
+        $signupDate = $user->created_at
+            ? Carbon::createFromTimestamp($user->created_at)->toDateString()
+            : '未知';
+
+        $message = sprintf(
+            "💰成功收款 %s元\n———————————————\n订单号：`%s`\n邮箱： `%s`\n套餐：%s\n类型：%s\n周期：%s\n优惠金额：%s\n优惠码：%s\n本次佣金：%s 元\n邀请人邮箱： `%s`\n%s\n注册日期：%s",
+            $order->total_amount / 100,
+            $order->trade_no,
+            $userEmail,
+            $planName,
+            $type,
+            $period,
+            $discountAmount,
+            $code,
+            $getAmount,
+            $inviterEmail,
+            $anotherInfo,
+            $signupDate
+        );
+        $telegramService = new TelegramService();
+        $telegramService->sendMessageWithAdmin($message,true);
+    }
+
+    private function getCommission($inviteUserId, $order)
+    {
+        $getAmount = 0;
+        $level = 3;
+        if ((int)config('v2board.commission_distribution_enable', 0)) {
+            $commissionShareLevels = [
+                0 => (int)config('v2board.commission_distribution_l1'),
+                1 => (int)config('v2board.commission_distribution_l2'),
+                2 => (int)config('v2board.commission_distribution_l3')
+            ];
+        } else {
+            $commissionShareLevels = [
+                0 => 100
+            ];
+        }
+        for ($l = 0; $l < $level; $l++) {
+            $inviter = User::find($inviteUserId);
+            if (!$inviter) continue;
+            if (!isset($commissionShareLevels[$l])) continue;
+            $getAmount = $order->commission_balance * ($commissionShareLevels[$l] / 100);
+            if (!$getAmount) continue;
+        }
+        return $getAmount / 100;
     }
 }
