057659daf3f518255569e62484bc9ab765a71d81
daley7292 <daley7292@gmail.com>
2025-05-14 16:48:56 +0800

    first commit

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf composer.phar
+wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
+php composer.phar install -vvv
+php artisan v2board:install
