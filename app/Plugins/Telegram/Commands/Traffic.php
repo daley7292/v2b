b3403d910807cc92b13875ddffec810ae78260fd
daley7292 <daley7292@gmail.com>
2025-05-16 17:50:41 +0800

    Update Traffic.php

--- app/Plugins/Telegram/Commands/Traffic.php
+++ app/Plugins/Telegram/Commands/Traffic.php
@@ -14,23 +14,6 @@ class Traffic extends Telegram
     public function handle($message, $match = [])
     {
         $telegramService = $this->telegramService;
-        if (!$message->is_private)
-            return;
-        $user = User::where('telegram_id', $message->chat_id)->first();
-        if (!$user) {
-            $telegramService->sendMessage($message->chat_id, '没有查询到您的用户信息，请先绑定账号', 'markdown');
-            return;
-        }
-        $transferEnable = Helper::trafficConvert($user->transfer_enable);
-        $up = Helper::trafficConvert($user->u);
-        $down = Helper::trafficConvert($user->d);
-        $remaining = Helper::trafficConvert($user->transfer_enable - ($user->u + $user->d));
-        $text = "🚥流量查询\n———————————————\n计划流量：`{$transferEnable}`\n已用上行：`{$up}`\n已用下行：`{$down}`\n剩余流量：`{$remaining}`";
-        $telegramService->sendMessage($message->chat_id, $text, 'markdown');
-    }
-    public function handle($message, $match = [])
-    {
-        $telegramService = $this->telegramService;
 
         // 获取发送者 ID
         $fromId = $message->from->id ?? null;
